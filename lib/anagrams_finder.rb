308ccc26bac177dd3f351ed617d089fda3ff4a87
Evangelos Giataganas <e.giataganas@gmail.com>
2014-07-13 21:45:52 +0100

    Create initialize method to work faster that read a url of words

--- lib/anagrams_finder.rb
+++ lib/anagrams_finder.rb
@@ -1,13 +1,29 @@
+require 'open-uri'
+
 class AnagramsFinder
+	
+	def initialize()
+		# f = File.new("wordlist.txt")
+		f = open('http://static.abscond.org/wordlist.txt')
+		@wordlist = f.readlines().map {|w| w.strip}
+	end
+
 	def find_anagrams(word)
 		words = []
-		f = File.new("wordlist.txt")
-		wordlist = f.readlines().map {|w| w.strip}
-		wordlist.each do |w| 
-			if word.length == w.length && word.chars.sort == w.chars.sort && w!=word
+		@wordlist.each do |w| 
+			if is_anagram?(word,w)
 				words.push(w)
 			end
 		end
 		words
 	end
+
+	def is_anagram?(word1,word2)
+		if word1.length == word2.length && word1.chars.sort == word2.chars.sort && word1!=word2
+			return true
+		else
+			return false
+		end
+	end
+
 end
