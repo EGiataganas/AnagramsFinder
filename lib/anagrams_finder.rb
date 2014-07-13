8a002bb91ad943530f97dcb9da33151ed4607429
Evangelos Giataganas <e.giataganas@gmail.com>
2014-07-13 22:42:55 +0100

    Separate Sinatra app created with some tests

--- lib/anagrams_finder.rb
+++ lib/anagrams_finder.rb
@@ -1,5 +1,3 @@
-require 'sinatra'
-require 'json'
 require 'open-uri'
 
 class AnagramsFinder
@@ -36,12 +34,4 @@ class AnagramsFinder
 		end
 	end
 
-end
-
-af = AnagramsFinder.new
-get '/:word' do
-	result = {}
-	words = params[:word].split(",")
-	words.each {|w| result[w] = af.find_anagrams(w)}
-	result.to_json
 end
