5a731c445c0ab8aad1be425476eb50d337c46408
Evangelos Giataganas <e.giataganas@gmail.com>
2014-07-13 21:30:21 +0100

    add the given word list to verify that everything is working

--- lib/anagrams_finder.rb
+++ lib/anagrams_finder.rb
@@ -1,7 +1,7 @@
 class AnagramsFinder
 	def find_anagrams(word)
 		words = []
-		f = File.new("shortwordlist.txt")
+		f = File.new("wordlist.txt")
 		wordlist = f.readlines().map {|w| w.strip}
 		wordlist.each do |w| 
 			if word.length == w.length && word.chars.sort == w.chars.sort && w!=word
